774870ac3b308451965d346ebdef77e6d2b4cc6c
Choudhary4 <157837187+Choudhary4@users.noreply.github.com>
2024-04-20 17:21:11 +0530

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,29 @@
+const User = require('../models/userModel.js');
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
+    await user.save();
+    res.send('User signed up successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Check if user exists in the database
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).send('Invalid email or password');
+    }
+    res.send('Login successful!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
